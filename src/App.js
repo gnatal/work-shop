7bf6d5b75d6998e4b4aae8270ab42c3d3b79a622
guilherme_victor_ramalho_natal <guilhermenatal47@gmail.com>
2020-05-16 15:28:26 -0300

    Instalado o router (nom install react-router-dom) e iniciado com props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React , {Component, Fragment} from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
-  render(){
-
+  render() {
     return (
-      <div className="App">
-        <Home></Home>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route component={() => <p> 404 Page not found</p>} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,69 +1,70 @@
-import React , {Component, Fragment } from 'react';
-import {Board, BoardCell, Mark, DefaultInput} from './style';
+import React, { Component, Fragment } from "react";
+import { Board, BoardCell, Mark, DefaultInput } from "./style";
 
-export default class Home extends Component{
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: ["", "", "", "", "", "", "", "", ""],
+      nome: "",
+      idade: 0,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            board: ['','','','K','','','','',''],
-            nome: "",
-            idade: 0
-        }
+  updateBlock = (index) => {
+    let board = this.state.board;
+
+    switch (board[index]) {
+      case "x":
+        board[index] = "O";
+        break;
+      case "O":
+        board[index] = "x";
+        break;
+      default:
+        board[index] = "x";
     }
 
-    updateBlock = (index) => {
-        let board = this.state.board;
-        
-        switch(board[index]){
-            case "x":
-                board[index] = "O";
-                break;
-            case "O":
-                board[index] = "x";
-                break;
-            default:
-                board[index] = "x";
-            }
+    this.setState({ board: board });
+  };
 
-        this.setState({board:board});
-    }
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
 
+    const { nome, idade, board } = this.state;
+    console.log("nome:", nome);
+    console.log("idade:", idade);
+    console.log("board:", board);
+  };
 
-    handleInput = (event) =>{
-        const { name, value} = event.target;
-        this.setState({[name]: value});
+  render() {
+    return (
+      <Fragment>
+        <p style={{ color: this.props.color }}>
+          i'm barbie girl in barbie world
+        </p>
+        <Board>
+          {this.state.board.map((value, index) => (
+            <BoardCell key={index} onClick={(e) => this.updateBlock(index)}>
+              <Mark>{value}</Mark>
+            </BoardCell>
+          ))}
+        </Board>
 
-        const {nome, idade, board} = this.state;
-        console.log("nome:", nome);
-        console.log("idade:", idade);
-        console.log("board:", board);
-    }
-
-    render()
-    {
-        return(
-            <Fragment>
-                <p>i'm barbie girl in barbie world</p>
-                <Board>
-                    {
-                        this.state.board.map((value, index) => (
-                        <BoardCell key={index} onClick={(e) => this.updateBlock(index)}>
-                            <Mark>
-                                {value}
-                            </Mark>
-                        </BoardCell>    
-                        ))
-                    }
-
-                </Board>
-
-                <Fragment>
-                    <DefaultInput placeholder="nome" name="nome"  onChange={this.handleInput}></DefaultInput>
-                    <input placeholder="idade" name="idade"  onChange={this.handleInput}></input>
-                </Fragment>
-            </Fragment>
-        );
-    }
-
+        <Fragment>
+          <DefaultInput
+            placeholder="nome"
+            name="nome"
+            onChange={this.handleInput}
+          ></DefaultInput>
+          <input
+            placeholder="idade"
+            name="idade"
+            onChange={this.handleInput}
+          ></input>
+        </Fragment>
+      </Fragment>
+    );
+  }
 }
